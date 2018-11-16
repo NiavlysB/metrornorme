1b4bb3a02217fbb0cbb60f120a6b6fe20617ace6
NiavlysB <sylvain.brunerie@gmail.com>
2018-11-17 00:06:00 +0100

    Add Metronome and Controls components : control the tempo before being able to hear it

---
+++ src/components/Controls.vue
@@ -0,0 +1,57 @@
+<template>
+   <div class="controls">
+      <div class="tempo-controls">
+         <button @click="slowDown()">−</button>
+         <div class="tempo">{{ tempo }}</div>
+         <button @click="speedUp()">+</button>
+      </div>
+      <div class="increment-controls">
+         <span>±</span>
+         <label v-for="value in possibleIncrements">
+            <input type="radio" v-model="increment" :value="value" />{{ value }}
+         </label>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   data () {
+      return {
+         increment: 1,
+         possibleIncrements: [1, 5, 10],
+      }
+   },
+   props: {
+      tempo: Number
+   },
+   methods: {
+      speedUp () {
+         this.$emit("change-tempo-by", this.increment)
+      },
+      slowDown () {
+         this.$emit("change-tempo-by", -this.increment)
+      },
+   }
+}
+</script>
+
+<style>
+.tempo-controls {
+   display: flex;
+   width: 100%;
+}
+
+.tempo-controls > * {
+   background: transparent;
+   color: currentColor;
+   font-size: inherit;
+   padding: .3em 1em;
+   flex: 1;
+}
+
+button {
+   cursor: pointer;
+   border: 1px solid #aaa;
+}
+</style>
---
+++ src/components/Metronome.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="Metronome">
+      <div class="visualization">[{{ tempo }}]</div>
+      <controls v-bind:tempo="tempo" v-on:change-tempo-by="changeTempoBy" />
+   </div>
+</template>
+
+<script>
+import Controls from "./Controls.vue"
+
+export default {
+   data () {
+      return {
+         tempo: 80
+      }
+   },
+   components: {
+      Controls
+   },
+   methods: {
+      setTempo (newTempo) {
+         this.tempo = newTempo
+      },
+      changeTempoBy (value) {
+         this.tempo += value
+      }
+   }
+}
+</script>
+
+<style>
+.Metronome {
+   border: .2em solid #333;
+   padding: 1.5em;
+}
+</style>
