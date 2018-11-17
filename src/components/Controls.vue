935a17beaf7b05f7fca0946525a555e8446babd8
NiavlysB <sylvain.brunerie@gmail.com>
2018-11-17 17:31:38 +0100

    Improve basic styling

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,9 +1,11 @@
 <template>
    <div class="tempo-controls">
       <div class="tempo-controls__tempo">
-         <button @click="slowDown()">−</button>
-         <div class="tempo">{{ tempo }}</div>
-         <button @click="speedUp()">+</button>
+         <button class="tempo-controls__tempo-btn"
+            @click="slowDown">−</button>
+         <div class="tempo-controls__tempo-value">{{ tempo }}</div>
+         <button class="tempo-controls__tempo-btn"
+            @click="speedUp">+</button>
       </div>
       <div class="tempo-controls__increment">
          <span aria-label="Incrémenter de" title="Incrémenter de">±</span>
@@ -56,6 +58,16 @@ export default {
    font-size: inherit;
    padding: .3em 1em;
    flex: 1;
+   line-height: 2em;
+   font-size: 1.3em;
+}
+
+.tempo-controls__tempo-value {
+   font-weight: bold;
+}
+
+.tempo-controls__increment {
+   margin-top: 1.5em;
 }
 
 button {
