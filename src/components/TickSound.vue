d8b99ad87222488f8222884dada16d1fdcb22e38
NiavlysB <sylvain.brunerie@gmail.com>
2018-12-19 19:31:35 +0100

    TickSound: no need to have audio elements in the DOM, just an HTMLAudioElement object

--- src/components/TickSound.vue
+++ src/components/TickSound.vue
@@ -1,29 +1,32 @@
 <template>
    <div class="audio-pool">
       <button @click="playTick">Tick</button>
-      <!--<audio ref="players" v-for="player in players" ></audio>-->
-      <audio ref="player" type="audio/wav" preload="auto" src="./src/assets/sounds/tick-1.wav"></audio>
    </div>
 </template>
 
 <script>
-// import tickSoundFile from "../assets/sounds/tick-1.ogg"
+import tickSoundFile from "../assets/sounds/tick-1.wav"
 
 export default {
    data () {
       return {
-         // tickSound: tickSoundFile
+         tickSound: null
       }
    },
    methods: {
 
       playTick () {
          console.log("Tick event received!")
-         this.$refs.player.play()
+         if (this.tickSound !== null) {
+            this.tickSound.play()
+         }
       }
 
    },
    mounted () {
+      this.tickSound = document.createElement("audio")
+      this.tickSound.src = tickSoundFile
+
       this.$on("tick", this.playTick)
    }
 }
